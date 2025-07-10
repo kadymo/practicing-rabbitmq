29a2aa58ac267f598f4e8b1a0cf1969ccc281604
Kádymo <santanakadymo@gmail.com>
2025-07-10 12:01:04 -0300

    Work Nodes, Acks and BasicQos

---
+++ NewTask/NewTask.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "task_queue", 
+    durable: true, 
+    exclusive: false, 
+    autoDelete: false, 
+    arguments: null
+);
+
+var properties = new BasicProperties
+{
+    Persistent = true
+};
+
+var message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(
+    exchange: string.Empty,
+    routingKey: "task_queue",
+    body
+);
+
+Console.WriteLine($" [x] Sent {message}");
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+};
