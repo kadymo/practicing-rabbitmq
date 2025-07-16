82737dfeb9ba392ff434e064e672e4ebeea13918
Kádymo <santanakadymo@gmail.com>
2025-07-16 11:16:25 -0300

    Emitting logs using a direct exchange with a severity routingKey

---
+++ EmitLogsDirect/EmitLogsDirect.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: "direct_logs", type: "direct");
+
+var severity = args.Length > 0 ? args[0] : "info";
+var message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+await channel.BasicPublishAsync(exchange: "direct_logs", routingKey: severity, body);
+
+Console.WriteLine($"[x] Sent '{severity}': '{message}'");
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+};
